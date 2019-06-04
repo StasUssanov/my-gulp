4546fff8ed3f8e8185bf32224400710e4cd4f17f
Станислав Усанов <s.ussanov@mail.ru>
2019-06-04 14:17:20 +0600

    #06-04-2

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,102 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass'); /* SCSS */
-const autoprefixer = require('gulp-autoprefixer'); /* Prefix CSS */
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify'); /* JavaScript */
-const notify = require('gulp-notify'); /* Оповещалка */
-const rename = require('gulp-rename'); /* Переименовалка */
+const gulp = require("gulp");
+const sass = require("gulp-sass"); /* SCSS */
+const autoprefixer = require("gulp-autoprefixer"); /* Prefix CSS */
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify"); /* JavaScript */
+const notify = require("gulp-notify"); /* Оповещалка */
+const rename = require("gulp-rename"); /* Переименовалка */
+
+const jsPath = [{ name: "js", dev: "__js", out: "../public/scripts" }];
+const scssPath = [{ name: "scss", dev: "__scss", out: "../public/css" }];
 
 /**
  * SCSS
  */
-gulp.task('sass', function() {
-  gulp
-    .src(['../__scss/*.scss', '!../__scss/_*.scss'])
-    /* Отлавливаем ошибки и сжимаем */
-    .pipe(
-      sass({ outputStyle: 'compressed' }).on(
-        'error',
-        notify.onError({
-          message: '<%= error.message %>',
-          title: 'scss - ошибка'
-        })
-      )
-    )
-    .pipe(autoprefixer({ browsers: ['last 2 versions'], cascade: false })) /* Добовляем префиксы */
-    .pipe(gulp.dest('../public/css')); /* Сохраняем */
+scssPath.forEach(element => {
+    gulp.task(element.name, function() {
+        gulp.src([
+            "../" + element.dev + "/*.scss",
+            "!../" + element.dev + "/_*.scss"
+        ])
+            /* Отлавливаем ошибки и сжимаем */
+            .pipe(
+                sass({ outputStyle: "compressed" }).on(
+                    "error",
+                    notify.onError({
+                        message: "<%= error.message %>",
+                        title: element.name + " - ошибка"
+                    })
+                )
+            )
+            .pipe(
+                autoprefixer({ browsers: ["last 2 versions"], cascade: false })
+            ) /* Добовляем префиксы */
+            .pipe(gulp.dest(element.out)); /* Сохраняем */
+    });
 });
 
 /**
  * JS
  */
-gulp.task('js', function() {
-  gulp
-    .src('../__js/*.js')
-    .pipe(
-      babel({
-        presets: ['@babel/preset-env'],
-        plugins: ['@babel/plugin-proposal-class-properties']
-      })
-    )
-    .pipe(
-      uglify({
-        toplevel: true,
-        ie8: true
-      })
-    )
-    .pipe(gulp.dest('../public/scripts'));
+jsPath.forEach(element => {
+    gulp.task(element.name, function() {
+        gulp.src([
+            "../" + element.dev + "/*.js",
+            "!../" + element.dev + "/_*.js"
+        ])
+            .pipe(
+                babel({
+                    presets: ["@babel/preset-env"],
+                    plugins: ["@babel/plugin-proposal-class-properties"]
+                })
+            )
+            .pipe(
+                uglify({
+                    toplevel: true,
+                    ie8: true
+                })
+            )
+            .pipe(gulp.dest(element.out));
+    });
 });
 
 /**
  * Vue
  */
-gulp.task('Vue', function() {
-  gulp
-    .src('../__js/*.vue')
-    .pipe(
-      uglify({
-        toplevel: true,
-        ie8: true
-      })
-    )
-    .pipe(
-      rename(function(path) {
-        path.basename += '-vue';
-        path.extname = '.js';
-      })
-    )
-    .pipe(gulp.dest('../public/scripts'));
+gulp.task("Vue", function() {
+    gulp.src("../__js/*.vue")
+        .pipe(
+            uglify({
+                toplevel: true,
+                ie8: true
+            })
+        )
+        .pipe(
+            rename(function(path) {
+                path.basename += "-vue";
+                path.extname = ".js";
+            })
+        )
+        .pipe(gulp.dest("../public/scripts"));
 });
 
 /**
  *	Отслеживаем изменения
  */
-gulp.task('default', function() {
-  gulp.watch('../__scss/*.scss').on('change', gulp.series('sass'));
-  gulp.watch('../__js/*.js').on('change', gulp.series('js'));
-  gulp.watch('../__js/*.vue').on('change', gulp.series('Vue'));
+gulp.task("default", function() {
+    jsPath.forEach(element => {
+        gulp.watch("../" + element.dev + "/*.js").on(
+            "change",
+            gulp.series(element.name)
+        );
+    });
+
+    scssPath.forEach(element => {
+        gulp.watch("../" + element.dev + "/*.scss").on(
+            "change",
+            gulp.series(element.name)
+        );
+    });
+
+    gulp.watch("../__js/*.vue").on("change", gulp.series("Vue"));
 });
