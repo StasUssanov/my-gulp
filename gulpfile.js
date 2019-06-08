32df3435c812c11f24e83d1aa3ab979b069e8742
Stanislav Ussanov <s.ussanov@mail.ru>
2019-06-08 10:43:09 +0600

    #fix: wait

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,10 @@ const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 //
 const notify = require('gulp-notify');
+const wait = require('gulp-wait');
 const rename = require('gulp-rename');
 const fileExists = require('file-exists');
+
 // Подключаем конфиг из проекта, если есть
 const config = fileExists.sync('../my-gulp-config.json')
   ? require('../my-gulp-config.json')
@@ -22,6 +24,7 @@ config.scss.forEach(element => {
   gulp.task(element.name, function() {
     gulp
       .src(['../' + element.dev + '/*.scss', '!../' + element.dev + '/_*.scss'])
+      .pipe(wait(1500))
       .pipe(
         sass({ outputStyle: 'compressed' }).on(
           'error',
@@ -44,6 +47,7 @@ config.js.forEach(element => {
   gulp.task(element.name, function() {
     gulp
       .src(['../' + element.dev + '/*.js', '!../' + element.dev + '/_*.js'])
+      .pipe(wait(1500))
       .pipe(
         babel({
           presets: ['@babel/preset-env'],
@@ -61,37 +65,13 @@ config.js.forEach(element => {
 });
 
 /**
- * Vue
- */
-gulp.task('Vue', function() {
-  gulp
-    .src('../__js/*.vue')
-    .pipe(
-      uglify({
-        toplevel: true,
-        ie8: true
-      })
-    )
-    .pipe(
-      rename(function(path) {
-        path.basename += '-vue';
-        path.extname = '.js';
-      })
-    )
-    .pipe(gulp.dest('../public/scripts'));
-});
-
-/**
  *	Отслеживаем изменения
  */
 gulp.task('default', function() {
   config.js.forEach(element => {
     gulp.watch('../' + element.dev + '/*.js').on('change', gulp.series(element.name));
   });
-
   config.scss.forEach(element => {
     gulp.watch('../' + element.dev + '/*.scss').on('change', gulp.series(element.name));
   });
-
-  gulp.watch('../__js/*.vue').on('change', gulp.series('Vue'));
 });
